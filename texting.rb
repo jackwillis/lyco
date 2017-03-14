b5a2a8e43f62f0f2f1b30ff1132b55f874ad70ac
Jack Willis <jack@attac.us>
2017-03-13 19:28:55 -0500

    clean up code

--- texting.rb
+++ texting.rb
@@ -1,24 +1,18 @@
 require "twilio-ruby"
 require "logger"
 
-################
-
-def rest_client
-  Twilio::REST::Client.new(ACCOUNT_SID, AUTH_TOKEN)
-end
-
 def send_sms!(to:, body:)
-  rest_client.account.messages.create(from: SENDER, to: to, body: body)
+  @_client ||= Twilio::REST::Client.new(ACCOUNT_SID, AUTH_TOKEN)
+  @_client.account.messages.create(from: SENDER, to: to, body: body)
 end  
-
-############
 
 def normalize_number(number)
   number.gsub(/\D/, "")
 end
 
 def parse_contacts(numbers_string)
-  numbers_string.split("\n")
+  numbers_string
+    .split("\n")
     .map { |c| normalize_number(c) }
     .reject(&:empty?)
     .uniq
