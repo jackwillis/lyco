34c6df4f60ad22ca306fe7ccc99119e6aa4a1a78
Jack Willis <jack@attac.us>
2017-04-27 15:12:49 -0500

    Rearrange settings code

--- settings.rb
+++ settings.rb
@@ -1,3 +1,42 @@
+# Twilio makes a request to this URL when
+# someone sends a text to our number
+get "/echo" do
+  from = params[:From]
+  body = params[:Body]
+
+  log = Logger.new(STDOUT)
+
+  log.info("#{from} replied: #{body}")
+
+  replies_forwardee = File.read(settings.replies_forwardee_filename).strip
+
+  send_sms!(to: replies_forwardee, body: "#{from}'s reply: #{body}")
+
+  log.info("#{from}'s message was forwarded to #{replies_forwardee}")
+
+  content_type :xml
+
+  response = Twilio::TwiML::Response.new do |r|
+    r.Sms Settings.automated_reply
+  end
+
+  response.text
+end
+
+get "/settings" do
+  @automated_reply = Settings.automated_reply
+  @replies_forwardee = Settings.replies_forwardee 
+
+  erb :settings
+end
+
+post "/settings" do
+  Settings.automated_reply = params[:automated_reply]
+  Settings.replies_forwardee = params[:replies_forwardee]
+
+  redirect "/settings"
+end
+
 module Settings
   AUTOMATED_REPLY_FILENAME = File.join(".", "data", "reply.txt")
   REPLIES_FORWARDEE_FILENAME = File.join(".", "data", "forwardee.txt")
@@ -34,44 +73,4 @@ module Settings
     File.write(REPLIES_FORWARDEE_FILENAME, forwardee.strip)
   end
 
-end
-
-# Twilio makes a request to this URL when
-# someone sends a text to our number
-get "/echo" do
-  from = params[:From]
-  body = params[:Body]
-
-  log = Logger.new(STDOUT)
-
-  log.info("#{from} replied: #{body}")
-
-  replies_forwardee = File.read(settings.replies_forwardee_filename).strip
-
-  send_sms!(to: replies_forwardee, body: "#{from}'s reply: #{body}")
-
-  log.info("#{from}'s message was forwarded to #{replies_forwardee}")
-
-  content_type :xml
-
-  response = Twilio::TwiML::Response.new do |r|
-    r.Sms Settings.automated_reply
-  end
-
-  response.text
-end
-
-get "/settings" do
-  @automated_reply = Settings.automated_reply
-  @replies_forwardee = Settings.replies_forwardee 
-  #File.read(settings.replies_forwardee_filename).strip
-
-  erb :settings
-end
-
-post "/settings" do
-  Settings.automated_reply = params[:automated_reply]
-  Settings.replies_forwardee = params[:replies_forwardee]
-
-  redirect "/settings"
 end
