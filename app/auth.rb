e15aa8bc78a65269538a94b8b5512c36febca784
Jack Willis <jack@attac.us>
2017-05-02 19:33:51 -0500

    Restore authentication to _all_ urls

--- app/auth.rb
+++ app/auth.rb
@@ -2,19 +2,6 @@ require "bcrypt"
 
 set :app_pass, BCrypt::Password.new(ENV["LYCO_SECRET"])
 
-class LycoAuth < Rack::Auth::Basic
-  def call(env)
-    request = Rack::Request.new(env)
-
-    case request.path
-    when "/echo"
-      @app.call(env) # skip basic auth on webhook url
-    else
-      super
-    end
-  end
-end
-
-use LycoAuth do |username, password|
+use Rack::Auth::Basic do |username, password|
   settings.app_pass == (username + ":" + password)
 end
